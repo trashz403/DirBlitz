e7d56f3938914d5e200a1d3f63f3384e56b8d845
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-06 17:56:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-# Copyright (c) 2023 GH05T-HUNTER5. All rights reserved.
 
-green=$'\e[1;32m'
-reset=$'\e[0m'
+green="\033[92m"
+red="\033[91m"
+white="\033[97m"
+reset="\033[0m"
+cyan="\033[36m"
+
 echo -e "${white}+-----------------------------------------------+"
 echo -e "${white}| ${green}Please wait for the base package installation ${white}|"
 echo -e "${white}+-----------------------------------------------+"
-install() {
-	if command -v python3 &>/dev/null; then
-		echo "Python package is already installed."
-	else
-		echo "Please install the Python3 package."
-		exit
-	fi
 
-	if pip3 install requests; then
-		echo "Requests package installed successfully."
-	else
-		echo "Failed to install requests package."
-		exit
-	fi
+install() {
+    if command -v python3 &>/dev/null; then
+        echo "Python package is already installed."
+    else
+        echo "Please install the Python3 package."
+        exit 1
+    fi
+
+    if pip3 install requests; then
+        echo "Requests package installed successfully."
+    else
+        echo "Failed to install requests package."
+        exit 1
+    fi
 }
 
 install
+
 wget https://gh05t-hunter5.github.io/the-source/Readers/requirements.sh && bash requirements.sh && rm -rf requirements.sh
+
 echo -e "${white}+-----------------------------------------------+"
 echo -e "${white}| ${green}Please wait for the base package installation ${white}|"
 echo -e "${white}+-----------------------------------------------+"
+
 mypass -l 8 1000 worldlist.txt
 clear
+
 echo -e "${white}+-----------------------------------------------+"
 echo -e "${white}|      ${green}Base packages installed successfully     ${white}|"
 echo -e "${white}+-----------------------------------------------+"
