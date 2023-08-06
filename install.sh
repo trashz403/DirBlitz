b1e80b5059c93d5a13cdb1dbb8693a8fe0164721
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-06 10:17:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Copyright (c) 2023 GH05T-HUNTER5. All rights reserved.
+
 green=$'\e[1;32m'
+reset=$'\e[0m'
+
 clear
+
 echo -e "${green}
  ██████╗ ██╗██████╗ ██████╗ ██╗     ██╗████████╗███████╗
  ██╔══██╗██║██╔══██╗██╔══██╗██║     ██║╚══██╔══╝╚══███╔╝
@@ -9,17 +13,26 @@ echo -e "${green}
  ██║  ██║██║██╔══██╗██╔══██╗██║     ██║   ██║    ███╔╝
  ██████╔╝██║██║  ██║██████╔╝███████╗██║   ██║   ███████╗
  ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝ 
-"
+${reset}"
+
 sleep 2
+
 install() {
-	if [[ $(command -v python3) ]]; then
-		echo "Python package installed successfully"
+	if command -v python3 &>/dev/null; then
+		echo "Python package is already installed."
 	else
-		echo "Please install the Python3 package"
+		echo "Please install the Python3 package."
 		exit
 	fi
-	pip install requests
+
+	if pip3 install requests; then
+		echo "Requests package installed successfully."
+	else
+		echo "Failed to install requests package."
+		exit
+	fi
 }
 
 install
+
 clear
